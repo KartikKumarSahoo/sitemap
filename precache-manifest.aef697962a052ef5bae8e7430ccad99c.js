12a44cbad403d21e6796fa13d182fc9cdd3ce0dd
Kartik Sahoo <kartikmca09@gmail.com>
2019-05-28 00:49:28 +0530

    Updates

---
+++ precache-manifest.aef697962a052ef5bae8e7430ccad99c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda80670797da16122cc26f4dff67043",
+    "url": "/sitemap/index.html"
+  },
+  {
+    "revision": "34555d70e6ef91a27caf",
+    "url": "/sitemap/static/css/main.c098c1b0.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/sitemap/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "34555d70e6ef91a27caf",
+    "url": "/sitemap/static/js/main.067fc607.chunk.js"
+  },
+  {
+    "revision": "d1665637a32c877b4f3c",
+    "url": "/sitemap/static/js/runtime~main.2e733d11.js"
+  }
+]);
